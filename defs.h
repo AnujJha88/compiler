62f15344298bc0b8b6d205cb91c95e39aeddc9f8
AnujJha88 <anujjha8788@gmail.com>
2025-08-11 10:05:58 +0530

    Abstract Syntax tree 1

--- defs.h
+++ defs.h
@@ -1,3 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
 struct Token{
     int token;
     int intvalue;
@@ -11,4 +16,17 @@ T_MULTIPLY,
 T_DIVIDE,
 T_INTLIT//when token is this the intvalue field holds value of integer we took in via scanner
 };
+
+enum{
+    A_ADD,A_SUB,A_MUL,A_DIV,A_INTLIT
+};
+
+struct ASTNode{
+    int op;//operator
+    struct ASTNode* left;
+    struct ASTNode* right;
+    int intvalue;//for INTLIT
+
+};
+
 
