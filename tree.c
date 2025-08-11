681da9dcacb043de38b7a322771636601a583a7e
AnujJha88 <anujjha8788@gmail.com>
2025-08-11 10:12:27 +0530

    making AST

---
+++ tree.c
@@ -0,0 +1,26 @@
+#include "defs.h"
+
+struct ASTNode *makeASTNode(int op, ASTNode* left, ASTNode*  right, int intvalue){
+struct ASTNode *node=(struct ASTNode*)malloc(sizeof(struct ASTNode)); // making new node in heap
+    if(!node){
+     fprintf(stderr,"Unable to make node \n");
+        exit(1);
+    }
+
+    node->op=op;
+    node->left=left;
+    node->right=right;
+    node->intvalue=intvalue;
+    return node;
+
+}
+
+// now we can use it to make our life easier by making some helper functions
+
+struct ASTNode *makeleaf(int op, int intvalue){
+    return makeASTNode(op,NULL,NULL,intvalue);
+}
+
+struct ASTNode *makeunary(int op, ASTNode* child,int intvalue){
+    return makeASTNode(op,child,NULL,intvalue);
+}
