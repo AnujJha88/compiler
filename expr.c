af9b7166abfcf04e839ecd673ae9656c9a21430f
AnujJha88 <anujjha8788@gmail.com>
2025-08-19 16:04:43 +0530

    add and mul expr

--- expr.c
+++ expr.c
@@ -38,20 +38,75 @@ int convert_token_to_AST_op(int token){
     }
 }
 
+static int arithop(int tokentype){
+    switch(tokentype){
+        case T_PLUS:
+        return A_ADD;
+        case T_MINUS:
+        return  A_SUB;
+        case T_MULTIPLY:
+        return A_MUL;
+        case T_DIVIDE:
+        return A_DIV;
+        default:
+            fprinf(stderr,"syntax error on line %d, token %d \n",Line,tokentype);
+        exit(1);
+    }
+}
+struct ASTNode* additive_expr();
+
+
+struct ASTNode* multiplicative_expr(){
+    struct ASTNode* left,*right;
+
+    int tokentype;
+
+    left=primary();
+    tokentype=Token.token;
+    if(tokentype==T_EOF){
+        return left;//end of file so no right component
+    }
+    while((tokentype==T_MULTIPLY)||(tokentype==T_DIVIDE)){
+        // fetch next integer
+        scan(&Token);
+        right=primary();
+    // join with the previous left tree to make the new left part
+        left=makeASTNode(arithop(tokentype),left,right,0);
+        // move on to the next token here
+        tokentype=Token.token;
+        if(tokentype==T_EOF) break;
+    }
+    return left;
+}
+
+
+struct ASTNode* additive_expr(){
+    // so what we have is either an additive expr with a multiplicative_expr
+    // or we have 2 mul expr with a + or - in between
+    struct ASTNode *left,*right;
+    int tokentype;
+    // setup
+    tokentype=Token.token;
+    left=multiplicative_expr();
+
+
+    if(tokentype==T_EOF)
+        return left;
+
+    while(true){
+scan(&Token);
+
+        right=multiplicative_expr();
+        left=makeASTNode(arithop(tokentype), left, right, 0);
+
+tokentype=Token.token;
+        if(tokentype==T_EOF) break;
+    }
+    return left;
+
+}
 // now we make the function that creates the actual tree
 //right now we have mo heirarchy between the operators
 struct ASTNode* binary_exp(){
-    struct ASTNode *left,*right,*node;
-        int nodetype;
-
-        left=primary();
-        if(Token.token ==T_EOF)return left;
-        nodetype=convert_token_to_AST_op(Token.token);// convert the token to an AST
-        // Node and then make it like the root of this subtree and then attach to
-        // the main root if any and yada yada
-        scan(&Token);
-        right=binary_exp();//so we leave the right node to be made
-        //recursively and we make each left subtree from every node 1 deep only
-        node=makeASTNode(nodetype,left,right,0);//this is not really having intvalues its having the operator so we do this
-        return node;
+return additive_expr();
 }
